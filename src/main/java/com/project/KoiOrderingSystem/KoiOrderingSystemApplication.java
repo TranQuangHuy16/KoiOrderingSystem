c83c5e5a7f02b34f4df574b0fbae89faf3d9672c
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2024-09-19 22:08:36 +0700

    First commit

---
+++ src/main/java/com/project/KoiOrderingSystem/KoiOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.KoiOrderingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/KoiOrderingSystem/KoiOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.KoiOrderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
