e22b607df66afb14aab3b1198aa4e797afa85d04
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2024-10-04 21:20:15 +0700

    sau lại security config

--- src/main/java/com/project/KoiOrderingSystem/config/SecurityConfig.java
+++ src/main/java/com/project/KoiOrderingSystem/config/SecurityConfig.java
@@ -56,6 +56,32 @@ public class SecurityConfig {
                 )
                 .userDetailsService(authenticationService)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(java.util.List.of("*"));
+                    corsConfiguration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(java.util.List.of("*"));
+                    return corsConfiguration;
+                }))
+                .build();
     }
+
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(
+//                        req -> req
+//                                .requestMatchers("/**")
+//                                .permitAll()
+//                                .anyRequest()
+//                                .authenticated()
+//
+//                )
+//                .userDetailsService(authenticationService)
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
+//    }
 }
